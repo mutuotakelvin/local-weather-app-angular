8a32695ed957d67c76f40018b1361645126f9cf8
mutuotakelvin <kelvinmutuota749@gmail.com>
2022-04-10 16:49:15 +0300

    applied flex layout align

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,14 +3,28 @@ import { Component } from '@angular/core'
 @Component({
   selector: 'app-root',
   template: `
-  <div style="text-align:center">
+  <div>
     <mat-toolbar color="primary">
       <span>WeatherMe</span>
     </mat-toolbar>
-    <div>Your city, your forecast, right now!</div>
-    <h2>Current Weather</h2>
-    <app-current-weather></app-current-weather>
-  </div>
+    <div fxLayoutAlign="center">
+      <div class="mat-caption">Your city, your forecast, right now!</div>
+    </div>
+    <div fxLayout="row">
+      <div fxFlex></div>
+        <mat-card fxFlex="300px">
+          <mat-card-header>
+            <mat-card-title>
+              <div class="mat-headline">Current Weather</div>
+            </mat-card-title>
+          </mat-card-header>
+          <mat-card-content>
+            <app-current-weather></app-current-weather>
+          </mat-card-content>
+        </mat-card>
+      <div fxFlex></div>
+    </div>
+</div>
   `,
   styleUrls: ['./app.component.css'],
 })
