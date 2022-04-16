64a8922196662dd53329bf57e1b86c0cd7b576c2
mutuotakelvin <kelvinmutuota749@gmail.com>
2022-04-16 17:49:50 +0300

    Refactor code o use updateCurrentWeather function

--- src/app/city-search/city-search.component.ts
+++ src/app/city-search/city-search.component.ts
@@ -21,10 +21,10 @@ export class CitySearchComponent implements OnInit {
       (searchValue: string)=> {
         if(!this.search.invalid){
           const userInput = searchValue.split(',').map(s => s.trim())
-        this.weatherService.getCurrentWeather(
+        this.weatherService.updateCurrentWeather(
           userInput[0],
-          userInput.length > 1 ? userInput[1] : ''
-        ).subscribe(data => (console.log(data)))
+          userInput.length > 1 ? userInput[1] : undefined
+        )
         }
       }
     )
