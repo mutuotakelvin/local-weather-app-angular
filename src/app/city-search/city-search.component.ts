8834a1673370043fd82f94067b83597688dfc03e
mutuotakelvin <kelvinmutuota749@gmail.com>
2022-04-16 11:14:39 +0300

    Import Validators from @angular/forms

--- src/app/city-search/city-search.component.ts
+++ src/app/city-search/city-search.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms'
+import { FormControl, Validators } from '@angular/forms'
 import { debounceTime } from 'rxjs/operators'
 
 import { WeatherService } from '../weather/weather.service';
